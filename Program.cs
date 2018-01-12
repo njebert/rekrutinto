e9e141971f0cd18a0c34ab604285f514fd9f0746
Nick Ebert <nick.ebert@LapkoSoft.local>
2018-01-11 23:10:33 -0600

    Added basic version of docker-compose file, running mongod process in docker container

--- Program.cs
+++ Program.cs
@@ -8,6 +8,11 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
+//https://github.com/Microsoft/generator-docker
+//https://medium.com/@arunoda/production-quality-mongodb-setup-with-docker-65136a4a9d8
+//https://coding4dummies.net/load-balanced-asp-net-core-application-with-docker-mongodb-and-redis-pt-4-825cb5f37241
+//https://kieldev.wordpress.com/2017/02/25/deploying-an-angular-2-net-core-1-1-web-api-and-mongodb-using-docker-containers/
+
 namespace rekrutinto
 {
     public class Program
